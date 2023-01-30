83f9b9b32477204949a7623ccfa41914faa3ebbc
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-01-30 18:35:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uçgen_çizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++) 
+            {
+                for (int j = 0; j < i; j++) 
+                {
+                    Console.Write("*"); 
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
